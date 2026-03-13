9662088c785a932c7aa72b6d61458b4a74dbcbd6
tbaev <teodor.baev@suse.com>
2026-03-13 09:36:29 +0200

    TDX MU validation

---
+++ tests/virt_autotest/tdx_validation.pm
@@ -0,0 +1,43 @@
+# SUSE's openQA tests
+#
+# Copyright 2025 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+#
+# Summary: Virtualization TDX guest verification test
+# Supports: SLES 16+ host (guests: SLES 16+)
+#
+# Test description:
+# This module tests whether TDX virtual machine has been successfully
+# installed on TDX enabled physical host by checking support status
+# on the host first and then on the virtual machine itself.
+#
+# Modes:
+# - Maintenance update: ENABLE_TDX=1
+# - Unified guest installation: VIRT_TDX_GUEST_INSTALL=1
+#
+# Maintainer: QE-Virtualization <qe-virt@suse.de> tbaev@suse.com
+
+package tdx_validation;
+
+use base 'virt_feature_test_base';
+use POSIX 'strftime';
+use testapi qw(:DEFAULT);
+use serial_terminal qw(select_serial_terminal);
+use virt_autotest::common;
+use virt_autotest::utils qw(guest_is_sle wait_guest_online is_guest_online execute_over_ssh upload_virt_logs);
+
+sub run_test {
+    my $self = shift;
+
+    record_info('TDX Test Started', 'TDX verification test started');
+}
+
+sub post_fail_hook {
+    my $self = shift;
+
+    record_info('Failure Hook', "Test failed, collecting logs for diagnosis");
+}
+
+1;
+
+
