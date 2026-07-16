912f4ae817a67db30255763c8aebd85547bdab1b
tbaev <teodor.baev@suse.com>
2026-07-16 10:14:17 +0300

    Debug larry with curl

--- tests/publiccloud/prepare_instance.pm
+++ tests/publiccloud/prepare_instance.pm
@@ -38,6 +38,10 @@ sub run {
     $instance_args{check_connectivity} = 0;    # Don't run wait_for_ssh() inside create_instance()
     $instance_args{use_extra_disk} = {size => $additional_disk_size, type => $additional_disk_type} if ($additional_disk_size > 0);
 
+    record_info('DEBUG', 'DEBUG');
+    assert_script_run("curl -k https://larry.qe.suse.de:444/");
+    assert_script_run("curl -4 -k https://larry.qe.suse.de:444/");
+    assert_script_run("curl -6 -k https://larry.qe.suse.de:444/");
     $args->{my_provider} = $self->provider_factory();
     $args->{my_instance} = $args->{my_provider}->create_instance(%instance_args);
     my $provider = $self->{my_provider} = $args->{my_provider};
